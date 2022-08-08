2965284bb0ef165c746af4d926e39548b632452b
soransh <57911107+soransh-singh@users.noreply.github.com>
2022-08-08 12:38:38 +0530

    update

--- src/components/Canvas/index.js
+++ src/components/Canvas/index.js
@@ -14,7 +14,7 @@ export default function Canvas({sketch, ...props}){
     }, []);
 
     return(
-            <div className={`canvas__container gallery__item--${props.size}`}>
+            <div className={`canvas__container`}>
                 <div ref={myRef}></div>
             </div>
     )
--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,15 +1,13 @@
+import { useState } from "react";
 import { sketch } from "../../Sketches/sketch";
 import Canvas from "../Canvas";
 import "./style.css"
 
 export default function Gallery(){
+    const [current, setCurrent] = useState(0);
     return(
         <div className="gallery">
-            <Canvas sketch={sketch[0]} size="large"/>
-            <Canvas sketch={sketch[1]} size="small"/>
-            <Canvas sketch={sketch[1]} size="large"/>
-            <Canvas sketch={sketch[1]} size="medium"/>
-            <Canvas sketch={sketch[1]} size="small"/>
+            <Canvas sketch={sketch[0]} />
         </div>
     )
 }
